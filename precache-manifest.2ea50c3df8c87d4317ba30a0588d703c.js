c767bde15dadbb2201efa41053fbcccb88ef101f
joanna.chen <ying0092@hotmail.com>
2020-07-29 22:04:54 +0800

    first commit

---
+++ precache-manifest.2ea50c3df8c87d4317ba30a0588d703c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b286c218a6277b1477",
+    "url": "/css/app.3e980162.css"
+  },
+  {
+    "revision": "52171e592a12c644065d",
+    "url": "/css/chunk-vendors.47543961.css"
+  },
+  {
+    "revision": "c6046eaf73259331916db1150e690c89",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "c5923022f5987e46afa1c19ee274dfc4",
+    "url": "/img/a1.c5923022.jpg"
+  },
+  {
+    "revision": "c76ab56250db924b64b26095e34a62bf",
+    "url": "/img/about-bg.c76ab562.jpg"
+  },
+  {
+    "revision": "928830b795a2a1cf596547150582af45",
+    "url": "/img/ba.928830b7.jpg"
+  },
+  {
+    "revision": "01daa339ecc40dd9efa41d85c8125367",
+    "url": "/img/bg-2_3-100.01daa339.jpg"
+  },
+  {
+    "revision": "458f2cbe9bb37732d278651114609be4",
+    "url": "/img/bg-n1.458f2cbe.jpg"
+  },
+  {
+    "revision": "5bb7991e605e562a83ea1ab06d81ed85",
+    "url": "/img/blog-bg.5bb7991e.jpg"
+  },
+  {
+    "revision": "c9bfdc764f733ac9a36bb49331a4869b",
+    "url": "/img/footer.c9bfdc76.svg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons 拷貝/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons 拷貝/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/img/icons 拷貝/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/img/icons 拷貝/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons 拷貝/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons 拷貝/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons 拷貝/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons 拷貝/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons 拷貝/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons 拷貝/apple-touch-icon.png"
+  },
+  {
+    "revision": "ba284ee3c06244110088f8400bc195d1",
+    "url": "/img/icons 拷貝/favicon-16x16.png"
+  },
+  {
+    "revision": "2918be270f9d045d23330c9d59bf1bd0",
+    "url": "/img/icons 拷貝/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons 拷貝/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons 拷貝/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/img/icons 拷貝/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "4299f32a825e7efac64ca74512476012",
+    "url": "/img/logo.4299f32a.svg"
+  },
+  {
+    "revision": "ce7e3562f096e6fef468345a904d53cb",
+    "url": "/img/ying.ce7e3562.jpg"
+  },
+  {
+    "revision": "61af2df0747d1e48683bcf372e714877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b286c218a6277b1477",
+    "url": "/js/app.9a791bd9.js"
+  },
+  {
+    "revision": "52171e592a12c644065d",
+    "url": "/js/chunk-vendors.b228f6e7.js"
+  },
+  {
+    "revision": "4d31a4dd8e5efc4f148671f916e18967",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
